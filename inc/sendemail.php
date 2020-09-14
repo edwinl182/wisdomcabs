00943fb80077393824b187600977f852477b4812
Salman khan <52947565+SalmanKhan-185NTsm@users.noreply.github.com>
2020-09-14 14:27:20 +0530

    contact us mail trigger

--- inc/sendemail.php
+++ inc/sendemail.php
@@ -1,8 +1,8 @@
 <?php
 
 // Define some constants
-define( "RECIPIENT_NAME", "John Doe" );
-define( "RECIPIENT_EMAIL", "mail@mail.com" );
+define( "RECIPIENT_NAME", "Wisdom Cabs" );
+define( "RECIPIENT_EMAIL", "reservations@wisdomcabs.com");
 
 // Read the form values
 $success = false;
@@ -10,19 +10,22 @@ $name = isset( $_POST['name'] ) ? preg_replace( "/[^\.\-\' a-zA-Z0-9]/", "", $_P
 $senderEmail = isset( $_POST['email'] ) ? preg_replace( "/[^\.\-\_\@a-zA-Z0-9]/", "", $_POST['email'] ) : "";
 $subject = isset( $_POST['subject'] ) ? preg_replace( "/[^\.\-\_\@a-zA-Z0-9]/", "", $_POST['subject'] ) : "";
 $message = isset( $_POST['message'] ) ? preg_replace( "/(From:|To:|BCC:|CC:|Subject:|Content-Type:)/", "", $_POST['message'] ) : "";
-
-print_R($_POST);
-exit();
+$contact_no = isset($_POST['phone']);
 
 
 $mail_subject = 'A contact request send by' . $name;
 
-$body = 'Name: '. $name . "\r\n";
-$body .= 'Email: '. $senderEmail . "\r\n";
-$body .= 'Subject: '. $subject . "\r\n";
-$body .= 'Message: '. "\r\n" . $message;
+// $body = 'Name: '. $name . "\r\n";
+// $body .= 'Email: '. $senderEmail . "\r\n";
+// $body .= 'Subject: '. $subject . "\r\n";
+// $body .= 'Message: '. "\r\n" . $message;
+$message_content = file_get_contents('./contact_template.html');
+$template_keys = array('%name%','%phone%','%message%');
+$replace_with = array($name,$contact_no,$message);
 
+$message_content = str_replace($template_keys,$replace_with,$message_content);
 
+$body = $message_content;
 
 // If all values exist, send the email
 if ( $name && $senderEmail && $message ) {
